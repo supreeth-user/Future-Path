6239cff580b340608037c5f8acbee29fd5964129
Nallandula Lakshmi Prasad Reddy <nallandula.lakshmi2025@vitstudent.ac.in>
2025-09-09 12:11:15 +0530

    Update Layout to include NavBar and Footer

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom';
+import { NavBar } from '../NavBar';
+import { Footer } from '../Footer';
 
 export function Layout() {
   return (
     <div>
-      {/* Header, navigation, etc. can be added here */}
-      <Outlet />
-      {/* Footer can be added here */}
+      <NavBar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 }
